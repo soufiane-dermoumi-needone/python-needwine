d01e75408d9d67139dcfef99530e0374bcc98238
NO-clegall <73113130+NO-clegall@users.noreply.github.com>
2020-11-10 16:32:54 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_script import Manager
 
 from app.main import create_app, db
 
-app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app = create_app(os.getenv('NEEDWINE_ENV') or 'dev')
 
 app.app_context().push()
 
