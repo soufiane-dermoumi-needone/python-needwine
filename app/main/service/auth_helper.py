cd4d62ae6a678cc5b8c7bd93dc51ec7622b1dab3
corentinlebris <corentin.lebris@needone.fr>
2020-11-19 15:18:24 +0100

    decorator token admin required

--- app/main/service/auth_helper.py
+++ app/main/service/auth_helper.py
@@ -55,3 +55,31 @@ class Auth:
                 'message': 'Provide a valid auth token.'
             }
             return response_object, 403
+
+    @staticmethod
+    def get_logged_in_user(new_request):
+        auth_token = new_request.get('Authorization')
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = User.query.filter_by(id=resp).first()
+                response_object = {
+                    'status': 'success',
+                    'data': {
+                        'user_id': user.id,
+                        'email': user.email,
+                        'role': user.role_id,
+                    } 
+                }
+                return response_object, 200
+            response_object = {
+                'status': 'fail',
+                'message': resp
+            }
+            return response_object, 401
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Provide a valid token.'
+            }
+            return response_object, 401
